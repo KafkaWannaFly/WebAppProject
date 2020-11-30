197d8fdb3b5cafa26526ac2cd6c3ca94675c12c0
Kafka Wanna Fly <47100782+tarzanchemgio@users.noreply.github.com>
2020-11-30 17:47:23 +0700

    Affort to Heroku 1.2

--- website-project/index.js
+++ website-project/index.js
@@ -5,12 +5,13 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "pages")));
-app.use("/resources", express.static(path.join(__dirname, "resources")));
+app.use(express.static(path.join(__dirname, "./pages")));
+app.use("/resources", express.static(path.join(__dirname, "./resources")));
 
 app.get("/", (req, res) => {
-	fs.readFile("index.html", (err, data) => {
-		res.header(200, { "content-type": "text/html" });
+	fs.readFile("index.htm", (err, data) => {
+		res.statusCode = 200;
+		res.setHeader("content-type", "text/html");
 		res.send(data);
 	});
 });
