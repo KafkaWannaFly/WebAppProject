ba9ba86ac14206f139b005503c62085d8f84f001
Phát Dương <69242061+beebapcay@users.noreply.github.com>
2020-12-11 17:53:52 +0700

    ReDesign + Responsive Mobile + Handerbar

--- index.js
+++ index.js
@@ -12,26 +12,26 @@ app.use("/resources", express.static(path.join(__dirname, "/resources")));
 
 // Set up for HandleBars
 app.engine(
-	"hbs",
-	hbs({
-		extname: "hbs",
-		defaultLayout: "default-layout",
-		layoutsDir: __dirname + "/Views/Layouts/",
-		partialsDir: __dirname + "/Views/Partials/",
-	})
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "default-layout",
+    layoutsDir: __dirname + "/Views/Layouts/",
+    partialsDir: __dirname + "/Views/Partials/",
+  })
 );
 
 app.set("view engine", "hbs");
 
 let item = {
-	id: "id",
-	name: "Saber’s Excalibur that Won 2 Holly Grail Wars",
-	category: {
-		categoryName: "Main Category",
-		fileName: "fileName",
-	},
-	description:
-		"Excalibur (/ɛkˈskælɪbər/) is the legendary sword of King Arthur,\
+  id: "id",
+  name: "Saber’s Excalibur that Won 2 Holly Grail Wars",
+  category: {
+    categoryName: "Main Category",
+    fileName: "fileName",
+  },
+  description:
+    "Excalibur (/ɛkˈskælɪbər/) is the legendary sword of King Arthur,\
                         sometimes also attributed with magical powers or\
                         associated with the rightful sovereignty of Britain. Excalibur and the Sword in the Stone (the\
                         proof of Arthur's \
@@ -40,60 +40,78 @@ let item = {
                         associated with the Arthurian legend very early on. In Welsh, it is called Caledfwlch; in \
                         Cornish, Calesvol (in Modern \
                         Cornish: Kalesvolgh); in Breton, Kaledvoulc'h; and in Latin, Caliburnus.",
-	imagePath: [
-		"https://images4.alphacoders.com/994/thumb-350-994514.jpg",
-		"https://images7.alphacoders.com/945/thumb-350-945610.png",
-		"https://images6.alphacoders.com/670/thumb-350-670517.png",
-	],
-	options: ["Short", "Medium", "Long"],
-	postedTime: new Date(2020, 20, 11),
-	price: 999999999,
-	stock: 20,
+  imagePath: [
+    "https://images4.alphacoders.com/994/thumb-350-994514.jpg",
+    "https://images7.alphacoders.com/945/thumb-350-945610.png",
+    "https://images6.alphacoders.com/670/thumb-350-670517.png",
+  ],
+  options: ["Short", "Medium", "Long"],
+  postedTime: new Date(2020, 20, 11),
+  price: 999999999,
+  stock: 20,
 };
 
 app.get("/", (req, res) => {
-	fs.readFile("index.htm", (err, data) => {
-		res.statusCode = 200;
-		res.setHeader("content-type", "text/html");
-		res.send(data);
-	});
+  res.render("index", { layout: "index-layout" });
 });
 
 app.get("/index.htm", (req, res) => {
-	fs.readFile("index.htm", (err, data) => {
-		res.statusCode = 200;
-		res.setHeader("content-type", "text/html");
-		res.send(data);
-	});
+  res.render("index", { layout: "index-layout" });
+});
+
+app.get("/login.htm", (req, res) => {
+  res.render("login", { layout: "login-layout" });
+});
+
+app.get("/sign-up.htm", (req, res) => {
+  res.render("sign-up", { layout: "sign-up-layout" });
+});
+
+app.get("/forgot-pwd.htm", (req, res) => {
+  res.render("forgot-pwd", { layout: "forgot-pwd-layout" });
+});
+
+app.get("/infor.htm", (req, res) => {
+  res.render("infor", { layout: "infor-layout" });
+});
+
+app.get("/order-list.htm", (req, res) => {
+  res.render("order-list", { layout: "order-list-layout" });
+});
+
+app.get("/list-product-template.htm", (req, res) => {
+  res.render("list-product-template", {
+    layout: "list-product-template-layout",
+  });
 });
 
 app.get("/item-detail.html", (req, res) => {
-	res.render("item-detail", { layout: "item-detail-layout", Item: item });
+  res.render("item-detail", { layout: "item-detail-layout", Item: item });
 });
 
 app.get("/pages/item-detail.html", (req, res) => {
-	res.render("item-detail", { layout: "item-detail-layout", Item: item });
+  res.render("item-detail", { layout: "item-detail-layout", Item: item });
 });
 
 app.get("/pages/:pageName", (req, res) => {
-	// console.log(`Page name: ${req.params["pageName"]}`);
-	let htmlPath = "./pages/" + req.params["pageName"];
+  // console.log(`Page name: ${req.params["pageName"]}`);
+  let htmlPath = "./pages/" + req.params["pageName"];
 
-	if (!fs.existsSync(htmlPath)) {
-		// console.log(`${req.params["pageName"]} not exist`);
-	}
+  if (!fs.existsSync(htmlPath)) {
+    // console.log(`${req.params["pageName"]} not exist`);
+  }
 
-	try {
-		fs.readFile(htmlPath, (err, data) => {
-			res.statusCode = 200;
-			res.setHeader("content-type", "text/html");
-			res.send(data);
-		});
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    fs.readFile(htmlPath, (err, data) => {
+      res.statusCode = 200;
+      res.setHeader("content-type", "text/html");
+      res.send(data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
-	console.log(`App is listening on https://usg-clothes.herokuapp.com:${port}`);
+  console.log(`App is listening on https://usg-clothes.herokuapp.com:${port}`);
 });
--- resources/js/script_ulti.js
+++ resources/js/script_ulti.js
@@ -1,3 +1,65 @@
-<script>
-	$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip()});
-</script>;
+//Script for tooltip
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+//Script for form search
+function showSearchForm() {
+  document.getElementById("nav-bar-menu").style.display = "none";
+  document.getElementById("form-search-nav").style.display = "block";
+}
+
+function closeSearchForm() {
+  document.getElementById("nav-bar-menu").style.display = "flex";
+  document.getElementById("form-search-nav").style.display = "none";
+}
+
+//Script for Valid Message
+// Disable form submissions if there are invalid fields
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Get the forms we want to add validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
+
+//Script For changeValue -> showRange
+function changeValueHeight(elementRange) {
+  document.getElementById("value-height").innerHTML = elementRange.value;
+}
+function changeValueWeight(elementRange) {
+  document.getElementById("value-weight").innerHTML = elementRange.value;
+}
+
+//Show Option changePwd
+function clickChangePwdHandler(checkBtn) {
+  if (checkBtn.checked == 1) {
+    document.getElementById("change-pwd-group").style.display = "block";
+  } else document.getElementById("change-pwd-group").style.display = "none";
+}
+
+//This script for add href product-template for header
+(function createHrefProductTemplate() {
+  var navMenuElement = document.querySelectorAll(".nav-link-menu ul li a");
+  for (let i = 0; i < navMenuElement.length; i++)
+    navMenuElement[i].href = "/list-product-template.htm";
+})();
