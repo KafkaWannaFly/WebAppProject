b14fdd77c5ad09a4ff4a1956134291fa943173f4
Kafka Wanna Fly <47100782+tarzanchemgio@users.noreply.github.com>
2020-12-16 11:03:30 +0700

    Fix Heroku 1.2

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 // app.use("/", express.static(path.join(__dirname, "../Views")));
 app.use("/pages", express.static(path.join(__dirname, "./pages")));
 app.use("/resources", express.static(path.join(__dirname, "/resources")));
+app.use("/views", express.static(path.join(__dirname, "/views")));
 
 // Set up for HandleBars
 app.engine(
@@ -20,7 +21,8 @@ app.engine(
 		partialsDir: __dirname + "/views/partials/",
 	})
 );
-app.set("views", path.join(__dirname, "/app/views"));
+
+// app.set("views", path.join(__dirname, "/views"));
 // app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
