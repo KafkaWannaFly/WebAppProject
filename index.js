cfbcba02db63acade4e7d7b4524c3258214d9133
Kafka Wanna Fly <47100782+tarzanchemgio@users.noreply.github.com>
2020-12-16 10:53:21 +0700

    Fix Heroku 1.0

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.engine(
 		partialsDir: __dirname + "/views/partials/",
 	})
 );
-
-app.set("views", __dirname + "/views");
+app.set("views", path.join(__dirname, "views"));
+// app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
 //Load Data Here
