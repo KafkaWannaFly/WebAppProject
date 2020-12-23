2d100ef98bc248b6184faa4a7af36b05641eb8e9
Kafka Wanna Fly <47100782+tarzanchemgio@users.noreply.github.com>
2020-12-23 08:27:42 +0700

    Update IndexRoute.js

--- routes/IndexRoute.js
+++ routes/IndexRoute.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-var controller = require("../controllers/IndexController");
+var controller = require("../controllers/IndexController.js");
 
 router.get("/", (req, res) => {
-  controller.getTopProduct().then((data) => {
-    res.render("index", { layout: "index-layout", TopProduct: data });
-  });
+	controller.getTopProduct().then((data) => {
+		res.render("index", { layout: "index-layout", TopProduct: data });
+	});
 });
 
 router.get("/index.htm", (req, res) => {
-  controller.getTopProduct().then((data) => {
-    res.render("index", { layout: "index-layout", TopProduct: data });
-  });
+	controller.getTopProduct().then((data) => {
+		res.render("index", { layout: "index-layout", TopProduct: data });
+	});
 });
 
 module.exports = router;
