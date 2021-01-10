331c13be37c0eb722b2ecb1fdc3fcb99df044a05
Phát Dương <69242061+beebapcay@users.noreply.github.com>
2021-01-10 16:37:49 +0700

    static admin UI

--- resources/js/Models/bill.ts
+++ resources/js/Models/bill.ts
@@ -6,13 +6,23 @@ interface BillItem {
 	amount: number;
 }
 
+enum PaymentMethod {
+	cod,
+	momo,
+	vnpay,
+	zalopay,
+}
+
 class Bill {
 	id: string = Date.now().toString(36) + Math.random().toString(36).substr(2);
 	billItems: BillItem[];
 	totalPrice?: number;
 	purchasedDate: Date = new Date();
+	phone: string;
 	customer?: User;
 	shippingAddress: string = "";
+	paymentMethod: PaymentMethod;
+	notes: string = "";
 }
 
 export { Bill, BillItem };
